90a08fa320f30820e4a1ee093b86b21b20cc4e90
Silvio Maia <mrmaiacodes@gmail.com>
2023-11-07 14:22:43 -0300

    file created

---
+++ src/test/java/br/com/instrumental_rental/InstrumentalRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.instrumental_rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstrumentalRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
