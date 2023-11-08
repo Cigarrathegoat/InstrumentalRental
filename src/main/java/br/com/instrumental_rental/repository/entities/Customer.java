d56d01350bec4d869218859c8a22a830ca9bb625
Silvio Maia <mrmaiacodes@gmail.com>
2023-11-08 14:47:18 -0300

    filled out instrument entity

--- src/main/java/br/com/instrumental_rental/repository/entities/Customer.java
+++ src/main/java/br/com/instrumental_rental/repository/entities/Customer.java
@@ -21,7 +21,7 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_CUSTOMER")
     @Column(name = "ID_CUSTOMER")
-    private String id;
+    private String customerId;
 
     @Column(name = "DS_NAME")
     private String name;
--- src/main/java/br/com/instrumental_rental/repository/entities/Instrument.java
+++ src/main/java/br/com/instrumental_rental/repository/entities/Instrument.java
@@ -1,4 +1,39 @@
 package br.com.instrumental_rental.repository.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "TB_INSTRUMENT")
+@SequenceGenerator(name = "SEQ_INSTRUMENT")
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Instrument {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_INSTRUMENT")
+    @Column(name = "ID_INSTRUMENT")
+    private String instrumentId;
+
+    @Column(name = "DS_MAKE")
+    private String make;
+
+    @Column(name = "DS_MODEL")
+    private String model;
+
+    @Column(name = "DS_PRICE")
+    private BigDecimal price;
+
+    @Column(name = "DS_MANUFACURE_DATE")
+    private LocalDate manufactureDate;
+
 }
