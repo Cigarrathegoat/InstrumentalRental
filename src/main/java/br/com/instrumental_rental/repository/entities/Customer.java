85bd8af52d85bb0185db23129a8a8879846fdec7
Silvio Maia <mrmaiacodes@gmail.com>
2023-11-08 14:39:43 -0300

    filled out customer entity

--- src/main/java/br/com/instrumental_rental/repository/entities/Customer.java
+++ src/main/java/br/com/instrumental_rental/repository/entities/Customer.java
@@ -1,4 +1,34 @@
 package br.com.instrumental_rental.repository.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "TB_CUSTOMER")
+@SequenceGenerator(name = "SEQ_CUSTOMER")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_CUSTOMER")
+    @Column(name = "ID_CUSTOMER")
+    private String id;
+
+    @Column(name = "DS_NAME")
+    private String name;
+
+    @Column(name = "DS_DATE_OF_BIRTH")
+    private LocalDate dateOfBirth;
+
+    @Column(name = "DS_ACCOUNT_BALANCE")
+    private BigDecimal accountBalance;
 }
