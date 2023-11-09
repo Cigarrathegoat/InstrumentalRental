2bcaf2e6af3b1f6ffed246238744908a5b0d15fe
Silvio Maia <mrmaiacodes@gmail.com>
2023-11-08 21:27:18 -0300

    ran attendant test method testSaveSuccess

---
+++ src/test/java/br/com/instrumental_rental/models/AttendantBuilder.java
@@ -0,0 +1,14 @@
+package br.com.instrumental_rental.models;
+
+import br.com.instrumental_rental.repository.entities.Attendant;
+import br.com.instrumental_rental.repository.entities.Customer;
+
+public class AttendantBuilder {
+    public static Attendant attendantBuilder(String id, String name) {
+        return Attendant.builder().attendantId(id).name(name).build();
+    }
+
+    public static Attendant attendantBuilderNoId(String name) {
+        return Attendant.builder().name(name).build();
+    }
+}
---
+++ src/test/java/br/com/instrumental_rental/service/AttendantServiceTest.java
@@ -0,0 +1,37 @@
+package br.com.instrumental_rental.service;
+
+import br.com.instrumental_rental.models.AttendantBuilder;
+import br.com.instrumental_rental.repository.entities.Attendant;
+import br.com.instrumental_rental.repository.interfaces.IAttendantRepository;
+import br.com.instrumental_rental.service.impl.AttendantService;
+import br.com.instrumental_rental.service.interfaces.IAttendantService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.when;
+
+public class AttendantServiceTest {
+
+    @Mock
+    IAttendantRepository attendantRepository;
+
+    @InjectMocks
+    AttendantService attendantService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);}
+
+    @Test
+    void testSaveSuccess() {
+        var builderNoId = AttendantBuilder.attendantBuilderNoId("Mark");
+        var builder = AttendantBuilder.attendantBuilder("1", "Mark");
+        when(attendantRepository.save(builderNoId)).thenReturn(builder);
+        Attendant saved = attendantService.save(builderNoId);
+        Assertions.assertNotNull(saved);
+    }
+}
