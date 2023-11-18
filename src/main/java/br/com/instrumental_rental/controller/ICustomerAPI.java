e35ffd76755534dee3ac9e53be0a15a4b774f77d
Silvio Maia <mrmaiacodes@gmail.com>
2023-11-18 18:08:50 -0300

    added addToBalance method in ICustomerAPI class

--- src/main/java/br/com/instrumental_rental/controller/ICustomerAPI.java
+++ src/main/java/br/com/instrumental_rental/controller/ICustomerAPI.java
@@ -1,11 +1,13 @@
 package br.com.instrumental_rental.controller;
 
+import br.com.instrumental_rental.controller.dto.requests.AccountBalanceDTO;
 import br.com.instrumental_rental.controller.dto.requests.CustomerDTO;
 import br.com.instrumental_rental.controller.dto.responses.errors.ErrorSpecificationDTO;
 import br.com.instrumental_rental.controller.dto.responses.responses.CustomerListResponseDTO;
 import br.com.instrumental_rental.controller.dto.responses.responses.CustomerResponseDTO;
 import br.com.instrumental_rental.controller.dto.responses.responses.DeleteResponseDTO;
 import br.com.instrumental_rental.exceptions.CustomerNotFoundException;
+import br.com.instrumental_rental.repository.entities.Customer;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -37,6 +39,12 @@ public interface ICustomerAPI {
     @ApiResponses(value = {@ApiResponse(code = 200, message = "success"),
     @ApiResponse(code = 404, response = ErrorSpecificationDTO.class, message = "not found")})
     public CustomerResponseDTO update() throws CustomerNotFoundException;
+
+    @ApiOperation(value = "add to accountBalance attribute in Customer object",
+    response = AccountBalanceDTO.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "success"),
+    @ApiResponse(code = 404, response = ErrorSpecificationDTO.class, message = "not found")})
+    public AccountBalanceDTO addToBalance() throws CustomerNotFoundException;
 
     @ApiOperation(value = "delete Customer object",
     response = CustomerDTO.class)
