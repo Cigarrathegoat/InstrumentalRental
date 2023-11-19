113906e150b7b066b4d3ef6a7f747f6cc6e32e6f
Silvio Maia <mrmaiacodes@gmail.com>
2023-11-19 18:34:54 -0300

    wrote delete method in IInstrumentAPI class

--- src/main/java/br/com/instrumental_rental/controller/IInstrumentAPI.java
+++ src/main/java/br/com/instrumental_rental/controller/IInstrumentAPI.java
@@ -2,12 +2,14 @@ package br.com.instrumental_rental.controller;
 
 import br.com.instrumental_rental.controller.dto.requests.InstrumentDTO;
 import br.com.instrumental_rental.controller.dto.responses.errors.ErrorSpecificationDTO;
+import br.com.instrumental_rental.controller.dto.responses.responses.DeleteResponseDTO;
 import br.com.instrumental_rental.controller.dto.responses.responses.InstrumentListResponseDTO;
 import br.com.instrumental_rental.controller.dto.responses.responses.InstrumentResponseDTO;
 import br.com.instrumental_rental.exceptions.InstrumentNotFoundException;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.http.ResponseEntity;
 
 public interface IInstrumentAPI {
 
@@ -34,5 +36,12 @@ public interface IInstrumentAPI {
     @ApiResponses(value = {@ApiResponse(code = 200, message = "success"),
     @ApiResponse(code = 404, response = ErrorSpecificationDTO.class, message = "not found")})
     public InstrumentResponseDTO update(String instrumentID, InstrumentDTO instrumentDTO)
+        throws InstrumentNotFoundException;
+
+    @ApiOperation(value = "delete Instrument object",
+    response = DeleteResponseDTO.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "success"),
+    @ApiResponse(code = 404, response = ErrorSpecificationDTO.class, message = "not found")})
+    public ResponseEntity<DeleteResponseDTO> delete(String instrumentId)
         throws InstrumentNotFoundException;
 }
