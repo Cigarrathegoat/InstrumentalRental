555a633142c3e9183ac2e961060eff06b7b97959
Silvio Maia <mrmaiacodes@gmail.com>
2023-11-19 18:54:44 -0300

    made add method in IRentalAPI class

--- src/main/java/br/com/instrumental_rental/controller/dto/IRentalAPI.java
+++ src/main/java/br/com/instrumental_rental/controller/dto/IRentalAPI.java
@@ -1,4 +1,19 @@
 package br.com.instrumental_rental.controller.dto;
 
+import br.com.instrumental_rental.controller.dto.requests.RentalDTO;
+import br.com.instrumental_rental.controller.dto.responses.errors.ErrorSpecificationDTO;
+import br.com.instrumental_rental.controller.dto.responses.responses.RentalResponseDTO;
+import br.com.instrumental_rental.exceptions.RentalNotFoundException;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 public interface IRentalAPI {
+
+    @ApiOperation(value = "add Rental object",
+    response = RentalResponseDTO.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message "success")
+    @ApiResponse(code = 404, response = ErrorSpecificationDTO.class, message = "not found")})
+    public RentalResponseDTO add(RentalDTO rentalDTO)
+        throws RentalNotFoundException;
 }
--- src/main/java/br/com/instrumental_rental/controller/dto/responses/responses/RentalResponseDTO.java
+++ src/main/java/br/com/instrumental_rental/controller/dto/responses/responses/RentalResponseDTO.java
@@ -6,7 +6,7 @@ import lombok.Data;
 
 @Data
 @Builder
-public class RentaResponseDTO {
+public class RentalResponseDTO {
 
     public RentalDTO data;
 }
