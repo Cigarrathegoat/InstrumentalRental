b7a6199d26265c962e7a544cdd4265cd63936a46
Silvio Maia <mrmaiacodes@gmail.com>
2023-11-20 11:41:32 -0300

    did findAll and update methods on IRentalAPI

--- src/main/java/br/com/instrumental_rental/controller/dto/IRentalAPI.java
+++ src/main/java/br/com/instrumental_rental/controller/dto/IRentalAPI.java
@@ -1,8 +1,8 @@
 package br.com.instrumental_rental.controller.dto;
 
 import br.com.instrumental_rental.controller.dto.requests.RentalDTO;
-import br.com.instrumental_rental.controller.dto.responses.errors.ErrorResponseDTO;
 import br.com.instrumental_rental.controller.dto.responses.errors.ErrorSpecificationDTO;
+import br.com.instrumental_rental.controller.dto.responses.responses.RentalListResponseDTO;
 import br.com.instrumental_rental.controller.dto.responses.responses.RentalResponseDTO;
 import br.com.instrumental_rental.exceptions.RentalNotFoundException;
 import io.swagger.annotations.ApiOperation;
@@ -22,5 +22,16 @@ public interface IRentalAPI {
     response = RentalResponseDTO.class)
     @ApiResponses(value = {@ApiResponse(code = 200, message = "success"),
     @ApiResponse(code = 404, response = ErrorSpecificationDTO.class, message = "not found")})
-    public RentalResponseDTO find(String rentalId);
+    public RentalResponseDTO find(String rentalId) throws RentalNotFoundException;
+
+    @ApiOperation(value = "find all Rental objects",
+    response = RentalResponseDTO.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "success")})
+    public RentalListResponseDTO findAll();
+
+    @ApiOperation(value = "update Rental objects",
+    response = RentalResponseDTO.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "success"),
+    @ApiResponse(code = 404, response = ErrorSpecificationDTO.class, message = "not found")})
+    public RentalResponseDTO update(String rentalId);
 }
