be9a914432c17663d0b7d3f61177c9e635366709
Silvio Maia <mrmaiacodes@gmail.com>
2023-11-20 11:45:32 -0300

    did delete method on IRentalAPI class

--- src/main/java/br/com/instrumental_rental/controller/dto/IRentalAPI.java
+++ src/main/java/br/com/instrumental_rental/controller/dto/IRentalAPI.java
@@ -2,12 +2,14 @@ package br.com.instrumental_rental.controller.dto;
 
 import br.com.instrumental_rental.controller.dto.requests.RentalDTO;
 import br.com.instrumental_rental.controller.dto.responses.errors.ErrorSpecificationDTO;
+import br.com.instrumental_rental.controller.dto.responses.responses.DeleteResponseDTO;
 import br.com.instrumental_rental.controller.dto.responses.responses.RentalListResponseDTO;
 import br.com.instrumental_rental.controller.dto.responses.responses.RentalResponseDTO;
 import br.com.instrumental_rental.exceptions.RentalNotFoundException;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.http.ResponseEntity;
 
 public interface IRentalAPI {
 
@@ -34,4 +36,11 @@ public interface IRentalAPI {
     @ApiResponses(value = {@ApiResponse(code = 200, message = "success"),
     @ApiResponse(code = 404, response = ErrorSpecificationDTO.class, message = "not found")})
     public RentalResponseDTO update(String rentalId);
+
+    @ApiOperation(value = "delete Rental object",
+    response = DeleteResponseDTO.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "success"),
+    @ApiResponse(code = 404, response = ErrorSpecificationDTO.class, message = "not found")})
+    public ResponseEntity<DeleteResponseDTO> delete(String rentalId)
+        throws RentalNotFoundException;
 }
