fe4574e5f24b60d364d090472791ffbfb3cbc3af
Silvio Maia <mrmaiacodes@gmail.com>
2023-11-19 22:32:00 -0300

    wrote find method on IRentalAPI class

--- src/main/java/br/com/instrumental_rental/controller/dto/IRentalAPI.java
+++ src/main/java/br/com/instrumental_rental/controller/dto/IRentalAPI.java
@@ -1,6 +1,7 @@
 package br.com.instrumental_rental.controller.dto;
 
 import br.com.instrumental_rental.controller.dto.requests.RentalDTO;
+import br.com.instrumental_rental.controller.dto.responses.errors.ErrorResponseDTO;
 import br.com.instrumental_rental.controller.dto.responses.errors.ErrorSpecificationDTO;
 import br.com.instrumental_rental.controller.dto.responses.responses.RentalResponseDTO;
 import br.com.instrumental_rental.exceptions.RentalNotFoundException;
@@ -12,8 +13,14 @@ public interface IRentalAPI {
 
     @ApiOperation(value = "add Rental object",
     response = RentalResponseDTO.class)
-    @ApiResponses(value = {@ApiResponse(code = 200, message "success")
+    @ApiResponses(value = {@ApiResponse(code = 200, message "success"),
     @ApiResponse(code = 404, response = ErrorSpecificationDTO.class, message = "not found")})
     public RentalResponseDTO add(RentalDTO rentalDTO)
         throws RentalNotFoundException;
+
+    @ApiOperation(value = "find Rental object",
+    response = RentalResponseDTO.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "success"),
+    @ApiResponse(code = 404, response = ErrorSpecificationDTO.class, message = "not found")})
+    public RentalResponseDTO find(String rentalId);
 }
