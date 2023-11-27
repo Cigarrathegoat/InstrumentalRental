671ede1f17ea34aafbb3e6e81dc8a60357d6e90a
Silvio Maia <mrmaiacodes@gmail.com>
2023-11-27 18:59:05 -0300

    did IInstrumentMapper mapper class

---
+++ src/main/java/br/com/instrumental_rental/Mappers/IInstrumentMapper.java
@@ -0,0 +1,18 @@
+package br.com.instrumental_rental.Mappers;
+
+import br.com.instrumental_rental.controller.dto.requests.InstrumentDTO;
+import br.com.instrumental_rental.repository.entities.Instrument;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper (componentModel = "spring")
+public interface IInstrumentMapper {
+
+    IInstrumentMapper INSTANCE = Mappers.getMapper(IInstrumentMapper.class);
+
+    InstrumentDTO convertToDTO(Instrument instrument);
+
+    List<InstrumentDTO> convertToListDTO(List<Instrument> instrumentList);
+}
