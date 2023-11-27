dc2e59bc69c0d2ab248d054886512140171420b8
Silvio Maia <mrmaiacodes@gmail.com>
2023-11-27 18:55:12 -0300

    created IAttendantMapper mapper class

---
+++ src/main/java/br/com/instrumental_rental/Mappers/IAttendantMapper.java
@@ -0,0 +1,20 @@
+package br.com.instrumental_rental.Mappers;
+
+import br.com.instrumental_rental.controller.dto.requests.AttendantDTO;
+import br.com.instrumental_rental.repository.entities.Attendant;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper (componentModel = "spring")
+public interface IAttendantMapper {
+
+    IAttendantMapper INSTANCE = Mappers.getMapper(IAttendantMapper.class);
+
+    AttendantDTO convertToDTO(Attendant attendant);
+
+    Attendant convertToEntity(AttendantDTO attendantDTO);
+
+    List<AttendantDTO> convertToListDto(List<Attendant> attendantList);
+}
--- src/main/java/br/com/instrumental_rental/controller/api/AttendantAPI.java
+++ src/main/java/br/com/instrumental_rental/controller/api/AttendantAPI.java
@@ -1,4 +1,38 @@
 package br.com.instrumental_rental.controller.api;
 
-public class AttendantAPI {
+import br.com.instrumental_rental.controller.IAttendantAPI;
+import br.com.instrumental_rental.controller.dto.requests.AttendantDTO;
+import br.com.instrumental_rental.controller.dto.requests.CustomerDTO;
+import br.com.instrumental_rental.controller.dto.responses.responses.AttendantListResponseDTO;
+import br.com.instrumental_rental.controller.dto.responses.responses.AttendantResponseDTO;
+import br.com.instrumental_rental.controller.dto.responses.responses.DeleteResponseDTO;
+import br.com.instrumental_rental.exceptions.AttendantNotFoundException;
+import br.com.instrumental_rental.exceptions.CustomerNotFoundException;
+import org.springframework.http.ResponseEntity;
+
+public class AttendantAPI implements IAttendantAPI {
+    @Override
+    public AttendantResponseDTO add(AttendantDTO attendantDTO) throws CustomerNotFoundException {
+        return null;
+    }
+
+    @Override
+    public AttendantListResponseDTO find(String attendantName) throws AttendantNotFoundException {
+        return null;
+    }
+
+    @Override
+    public AttendantListResponseDTO findAll() {
+        return null;
+    }
+
+    @Override
+    public AttendantResponseDTO update(Long attendantId, CustomerDTO customerDTO) throws AttendantNotFoundException {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<DeleteResponseDTO> delete(Long attendantId) throws AttendantNotFoundException {
+        return null;
+    }
 }
