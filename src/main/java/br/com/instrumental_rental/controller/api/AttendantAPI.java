8cf3b66ad44770c4a876e940d9ad85901edb8eb6
Silvio Maia <mrmaiacodes@gmail.com>
2024-01-08 22:47:03 -0300

    did some stuff on AttendantAPI

--- src/main/java/br/com/instrumental_rental/controller/api/AttendantAPI.java
+++ src/main/java/br/com/instrumental_rental/controller/api/AttendantAPI.java
@@ -1,5 +1,6 @@
 package br.com.instrumental_rental.controller.api;
 
+import br.com.instrumental_rental.Mappers.IAttendantMapper;
 import br.com.instrumental_rental.controller.IAttendantAPI;
 import br.com.instrumental_rental.controller.dto.requests.AttendantDTO;
 import br.com.instrumental_rental.controller.dto.requests.CustomerDTO;
@@ -8,9 +9,21 @@ import br.com.instrumental_rental.controller.dto.responses.responses.AttendantRe
 import br.com.instrumental_rental.controller.dto.responses.responses.DeleteResponseDTO;
 import br.com.instrumental_rental.exceptions.AttendantNotFoundException;
 import br.com.instrumental_rental.exceptions.CustomerNotFoundException;
+import br.com.instrumental_rental.service.interfaces.IAttendantService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
 public class AttendantAPI implements IAttendantAPI {
+
+    private IAttendantService attendantService;
+
+    private IAttendantMapper attendantMapper;
+
+    @Autowired
+    public AttendantAPI(IAttendantService attendantService, IAttendantMapper attendantMapper) {
+        this.attendantMapper = attendantMapper;
+        this.attendantService = attendantService;
+    }
     @Override
     public AttendantResponseDTO add(AttendantDTO attendantDTO) throws CustomerNotFoundException {
         return null;
