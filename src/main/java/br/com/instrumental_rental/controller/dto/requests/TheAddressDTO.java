3becef838b10917f321c3e2384c1119c67fe416a
Silvio Maia <mrmaiacodes@gmail.com>
2024-01-08 22:34:19 -0300

    did some more stuff in this class

--- src/main/java/br/com/instrumental_rental/controller/dto/requests/TheAddressDTO.java
+++ src/main/java/br/com/instrumental_rental/controller/dto/requests/TheAddressDTO.java
@@ -3,13 +3,23 @@ package br.com.instrumental_rental.controller.dto.requests;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+
 @Data
 @Builder
 public class TheAddressDTO {
 
+    @NotBlank(message = "field must not be blank")
+    @Pattern(regexp  = "^[a-zA-Z]+$", message = "field must be letters only")
     private String streetName;
 
+    @NotBlank(message = "field must not be blank")
+    @Pattern(regexp = "^[0-9]+$", message = "field must be numbers only")
+    @Positive(message = "field must be a positive number")
     private String streetNumber;
+
 
     private String borough;
 
