77fde2bd4d13fd7f239599b1315e6e913129b9e8
MrMaia <mrmaiacodes@gmail.com>
2024-01-19 13:22:43 -0300

    started TheAddressServiceTest

---
+++ src/test/java/br/com/instrumental_rental/service/TheAddressServiceTest.java
@@ -0,0 +1,23 @@
+package br.com.instrumental_rental.service;
+
+import br.com.instrumental_rental.repository.interfaces.ITheAddressRepository;
+import br.com.instrumental_rental.service.impl.TheAddressService;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class TheAddressServiceTest {
+
+    @Mock
+    ITheAddressRepository theAddressRepository;
+
+    @InjectMocks
+    TheAddressService addressService;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+}
