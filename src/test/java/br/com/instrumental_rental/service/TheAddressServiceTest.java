80de039e84ce710e18eb88b6598cbb90c5d27d4d
MrMaia <mrmaiacodes@gmail.com>
2024-01-27 21:49:48 -0300

    Successfully finished findAllSuccess method in TheAddressService test class

--- src/test/java/br/com/instrumental_rental/service/TheAddressServiceTest.java
+++ src/test/java/br/com/instrumental_rental/service/TheAddressServiceTest.java
@@ -12,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.when;
@@ -50,7 +51,7 @@ public class TheAddressServiceTest {
     @Test
     void findByIdTheAddressNotFoundException() throws TheAddressNotFoundException {
     var builder = TheAddressBuilder.theAddressBuilder();
-    when(theAddressRepository.findById(builder.getAddressId())).thenReturn(Optional.of(null));
+    when(theAddressRepository.findById(builder.getAddressId())).thenReturn(Optional.empty());
     TheAddressNotFoundException thrown = Assertions.assertThrows(
             TheAddressNotFoundException.class, () -> {
                 theAddressService.findById(builder.getAddressId());}
@@ -59,5 +60,14 @@ public class TheAddressServiceTest {
         Assertions.assertEquals("Address not found", thrown.getMessage());
     }
 
+    @Test
+    void findAllSuccess() {
+        var builder = TheAddressBuilder.theAddressBuilder();
+        when(theAddressRepository.findAll()).thenReturn(List.of(builder));
+        List<TheAddress> result = theAddressService.findAll();
+        Assertions.assertEquals(List.of(builder), result);
+    }
 
+    @Test
+    void updateSuccess() throws TheAddressNo
 }
