a83146ed86bdf4447b4c34db15e6b2df35a42118
MrMaia <mrmaiacodes@gmail.com>
2024-01-28 21:47:49 -0300

    successfully ran testSaveSuccess test method on ContactServiceTest class

--- src/test/java/br/com/instrumental_rental/models/ContactBuilder.java
+++ src/test/java/br/com/instrumental_rental/models/ContactBuilder.java
@@ -4,14 +4,14 @@ import br.com.instrumental_rental.repository.entities.Contact;
 
 public class ContactBuilder {
 
-    public Contact contactBuilder() {
+    public static Contact contactBuilder() {
         return Contact.builder()
                 .contactId(1L)
                 .contactType("email")
                 .contactContent("contact@gmail.com").build();
     }
 
-    public Contact contactNoIdBuilder() {
+    public static Contact contactNoIdBuilder() {
         return Contact.builder()
                 .contactId(null)
                 .contactType("email")
--- src/test/java/br/com/instrumental_rental/service/ContactServiceTest.java
+++ src/test/java/br/com/instrumental_rental/service/ContactServiceTest.java
@@ -1,10 +1,17 @@
 package br.com.instrumental_rental.service;
 
+import br.com.instrumental_rental.models.ContactBuilder;
+import br.com.instrumental_rental.repository.entities.Contact;
 import br.com.instrumental_rental.repository.interfaces.IContactRepository;
+import br.com.instrumental_rental.service.impl.ContactService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.when;
 
 public class ContactServiceTest {
 
@@ -12,10 +19,18 @@ public class ContactServiceTest {
     IContactRepository contactRepository;
 
     @InjectMocks
-    ContactServiceTest contactService;
+    ContactService contactService;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testSaveSuccess() {
+        var builder = ContactBuilder.contactBuilder();
+        when(contactRepository.save(builder)).thenReturn(builder);
+        Contact save = contactService.save(builder);
+        Assertions.assertEquals(builder, save);
     }
 }
