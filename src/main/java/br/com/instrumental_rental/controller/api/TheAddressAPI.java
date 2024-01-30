170308b0b129ad7f223005d6a11acf6981dc6e30
MrMaia <mrmaiacodes@gmail.com>
2024-01-29 22:15:15 -0300

    did add method in TheAddressAPI class

--- src/main/java/br/com/instrumental_rental/controller/api/TheAddressAPI.java
+++ src/main/java/br/com/instrumental_rental/controller/api/TheAddressAPI.java
@@ -1,20 +1,41 @@
 package br.com.instrumental_rental.controller.api;
 
+import br.com.instrumental_rental.Mappers.ITheAddressMapper;
 import br.com.instrumental_rental.controller.ITheAddressAPI;
 import br.com.instrumental_rental.controller.dto.requests.TheAddressDTO;
 import br.com.instrumental_rental.controller.dto.responses.responses.DeleteResponseDTO;
 import br.com.instrumental_rental.controller.dto.responses.responses.TheAddressResponseDTO;
 import br.com.instrumental_rental.exceptions.TheAddressNotFoundException;
+import br.com.instrumental_rental.service.interfaces.ITheAddressService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("V1/TheAddress")
 public class TheAddressAPI implements ITheAddressAPI {
-    @
-    public TheAddressResponseDTO add(TheAddressDTO theAddressDTO) {
-        return null;
+
+    private ITheAddressService theAddressService;
+
+    private ITheAddressMapper theAddressMapper;
+
+    public TheAddressAPI(ITheAddressService theAddressService, ITheAddressMapper theAddressMapper) {
+        this.theAddressMapper = theAddressMapper;
+        this.theAddressService = theAddressService;
+    }
+
+    @PostMapping("/add")
+    public TheAddressResponseDTO add(@RequestBody TheAddressDTO theAddressDTO) {
+        return TheAddressResponseDTO.builder()
+                .data(theAddressMapper.convertToDto(
+                        theAddressService.save(
+                                theAddressMapper.convertToEntity(
+                                        theAddressDTO)
+                        )
+                )
+                ).build();
     }
 
     @Override
