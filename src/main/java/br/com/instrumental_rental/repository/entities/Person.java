f04edf530e30894f845d9122d6a8b0950946f994
MrMaia <mrmaiacodes@gmail.com>
2024-01-30 20:00:42 -0300

    filled out person entity

--- src/main/java/br/com/instrumental_rental/repository/entities/Person.java
+++ src/main/java/br/com/instrumental_rental/repository/entities/Person.java
@@ -2,9 +2,9 @@ package br.com.instrumental_rental.repository.entities;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "TB_PERSON")
@@ -16,4 +16,29 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @EqualsAndHashCode
 public class Person {
+
+    /
+
+    @Column(name = "DS_NAME")
+    private String name;
+
+    @Column(name = "DS_DATE_OF_BIRTH")
+    private LocalDate dateOfBirth;
+
+    @Column(name = "DS_SOCIAL_SECURITY")
+    private String socialSecurityNumber;//9 digits
+
+    @Column(name = "DS_DRIVERS-LICENSE")
+    private String driversLicenseNumber;//7 digits
+
+    @Column(name = "DS_RENTAL")
+    @JoinColumn(name = "RENTAL_DS")
+    @OneToMany(mappedBy = "attendant")
+    private Rental rental;
+
+    @Column(name = "DS_CONTACTS")
+    @JoinColumn(name = "CONTACTS_DS")
+    @OneToMany(mappedBy = "customer")
+    private List<Contact> contacts;
+
 }
