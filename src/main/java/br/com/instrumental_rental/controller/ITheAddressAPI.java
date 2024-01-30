01cf7e201df7dc4e876d89d8c1b2920ce5f9f000
MrMaia <mrmaiacodes@gmail.com>
2024-01-29 21:16:05 -0300

    finished find method in ITheAddressAPI class

--- src/main/java/br/com/instrumental_rental/controller/ITheAddressAPI.java
+++ src/main/java/br/com/instrumental_rental/controller/ITheAddressAPI.java
@@ -2,6 +2,8 @@ package br.com.instrumental_rental.controller;
 
 import br.com.instrumental_rental.controller.dto.requests.TheAddressDTO;
 import br.com.instrumental_rental.controller.dto.responses.errors.ErrorSpecificationDTO;
+import br.com.instrumental_rental.controller.dto.responses.responses.TheAddressResponseDTO;
+import br.com.instrumental_rental.exceptions.TheAddressNotFoundException;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -20,4 +22,10 @@ public interface ITheAddressAPI {
     @ApiResponses(value = {@ApiResponse(code = 200, message = "success"),
     @ApiResponse(code = 404, response = ErrorSpecificationDTO.class, message = "not found")})
     TheAddressResponseDTO add(TheAddressDTO theAddressDTO);
+
+    @ApiOperation(value = "finds TheAddress object",
+    response = TheAddressResponseDTO.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "success"),
+    @ApiResponse(code = 400, response = ErrorSpecificationDTO.class, message = "not found")})
+    TheAddressResponseDTO find(Long theAddressID) throws TheAddressNotFoundException;
 }
