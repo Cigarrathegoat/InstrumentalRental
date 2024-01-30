57861e687ee87f85eb608842c6a771b5a5109086
MrMaia <mrmaiacodes@gmail.com>
2024-01-29 21:32:22 -0300

    filled out TheAddressAPI class

--- src/main/java/br/com/instrumental_rental/controller/ITheAddressAPI.java
+++ src/main/java/br/com/instrumental_rental/controller/ITheAddressAPI.java
@@ -5,20 +5,12 @@ import br.com.instrumental_rental.controller.dto.responses.errors.ErrorSpecifica
 import br.com.instrumental_rental.controller.dto.responses.responses.DeleteResponseDTO;
 import br.com.instrumental_rental.controller.dto.responses.responses.TheAddressResponseDTO;
 import br.com.instrumental_rental.exceptions.TheAddressNotFoundException;
-import br.com.instrumental_rental.repository.entities.TheAddress;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 
 public interface ITheAddressAPI {
-
-    /* @ApiOperation(value = "add Customer object",
-    response = CustomerDTO.class)
-    @ApiResponses(value = {@ApiResponse(code = 200, message = "success"),
-    @ApiResponse(code =404, response = ErrorSpecificationDTO.class, message = "not found")})
-    CustomerResponseDTO add(CustomerDTO customerDTO);
-     */
 
     @ApiOperation(value = "add TheAddress object",
     response = TheAddressResponseDTO.class)
@@ -37,14 +29,6 @@ public interface ITheAddressAPI {
     @ApiResponses(value = {@ApiResponse(code = 200, message = "success"),
     @ApiResponse(code = 400, response = ErrorSpecificationDTO.class, message = "not found")})
     TheAddressResponseDTO update(Long theAddressID) throws TheAddressNotFoundException;
-
-    /*@ApiOperation(value = "delete Contacts object",
-    response = DeleteResponseDTO.class)
-    @ApiResponses(value = {@ApiResponse(code = 200, message = "success"),
-    @ApiResponse(code = 404, response = ErrorSpecificationDTO.class, message = "not found")})
-    ResponseEntity<DeleteResponseDTO> delete(Long contactsId) throws ContactNotFoundException;
-
-     */
 
     @ApiOperation(value = "delete TheAddress object",
     response = DeleteResponseDTO.class)
--- src/main/java/br/com/instrumental_rental/controller/api/TheAddressAPI.java
+++ src/main/java/br/com/instrumental_rental/controller/api/TheAddressAPI.java
@@ -1,9 +1,34 @@
 package br.com.instrumental_rental.controller.api;
 
+import br.com.instrumental_rental.controller.ITheAddressAPI;
+import br.com.instrumental_rental.controller.dto.requests.TheAddressDTO;
+import br.com.instrumental_rental.controller.dto.responses.responses.DeleteResponseDTO;
+import br.com.instrumental_rental.controller.dto.responses.responses.TheAddressResponseDTO;
+import br.com.instrumental_rental.exceptions.TheAddressNotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("V1/TheAddress")
-public class TheAddressAPI {
+public class TheAddressAPI implements ITheAddressAPI {
+    @
+    public TheAddressResponseDTO add(TheAddressDTO theAddressDTO) {
+        return null;
+    }
+
+    @Override
+    public TheAddressResponseDTO find(Long theAddressID) throws TheAddressNotFoundException {
+        return null;
+    }
+
+    @Override
+    public TheAddressResponseDTO update(Long theAddressID) throws TheAddressNotFoundException {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<DeleteResponseDTO> delete(Long theAddressID) throws TheAddressNotFoundException {
+        return null;
+    }
 }
