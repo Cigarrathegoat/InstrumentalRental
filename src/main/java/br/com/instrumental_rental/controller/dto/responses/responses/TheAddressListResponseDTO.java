fa711844f113b2e5cbdaa13dd63d1989868cd148
MrMaia <mrmaiacodes@gmail.com>
2024-01-29 21:12:33 -0300

    finished TheAddressListResponseDTO class

--- src/main/java/br/com/instrumental_rental/controller/dto/responses/responses/TheAddressListResponseDTO.java
+++ src/main/java/br/com/instrumental_rental/controller/dto/responses/responses/TheAddressListResponseDTO.java
@@ -1,9 +1,14 @@
 package br.com.instrumental_rental.controller.dto.responses.responses;
 
+import br.com.instrumental_rental.controller.dto.requests.TheAddressDTO;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @Builder
 public class TheAddressListResponseDTO {
+
+    public List<TheAddressDTO> data;
 }
