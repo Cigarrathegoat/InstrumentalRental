819a439eef1729a4a1e293819863ebc06bf27d97
MrMaia <mrmaiacodes@gmail.com>
2024-01-29 21:30:22 -0300

    did delete method in ITheAddressAPI class

--- src/main/java/br/com/instrumental_rental/controller/ITheAddressAPI.java
+++ src/main/java/br/com/instrumental_rental/controller/ITheAddressAPI.java
@@ -2,11 +2,14 @@ package br.com.instrumental_rental.controller;
 
 import br.com.instrumental_rental.controller.dto.requests.TheAddressDTO;
 import br.com.instrumental_rental.controller.dto.responses.errors.ErrorSpecificationDTO;
+import br.com.instrumental_rental.controller.dto.responses.responses.DeleteResponseDTO;
 import br.com.instrumental_rental.controller.dto.responses.responses.TheAddressResponseDTO;
 import br.com.instrumental_rental.exceptions.TheAddressNotFoundException;
+import br.com.instrumental_rental.repository.entities.TheAddress;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.http.ResponseEntity;
 
 public interface ITheAddressAPI {
 
@@ -34,4 +37,18 @@ public interface ITheAddressAPI {
     @ApiResponses(value = {@ApiResponse(code = 200, message = "success"),
     @ApiResponse(code = 400, response = ErrorSpecificationDTO.class, message = "not found")})
     TheAddressResponseDTO update(Long theAddressID) throws TheAddressNotFoundException;
+
+    /*@ApiOperation(value = "delete Contacts object",
+    response = DeleteResponseDTO.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "success"),
+    @ApiResponse(code = 404, response = ErrorSpecificationDTO.class, message = "not found")})
+    ResponseEntity<DeleteResponseDTO> delete(Long contactsId) throws ContactNotFoundException;
+
+     */
+
+    @ApiOperation(value = "delete TheAddress object",
+    response = DeleteResponseDTO.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "success"),
+    @ApiResponse(code = 400, response = ErrorSpecificationDTO.class, message = "not found")})
+    ResponseEntity<DeleteResponseDTO> delete(Long theAddressID) throws TheAddressNotFoundException;
 }
