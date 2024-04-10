8d860ea8849c2ea463b64248c2ce8c00b98a0982
MrMaia <mrmaiacodes@gmail.com>
2024-04-10 19:38:00 -0300

    started ContactsAPI class, wrote save method

---
+++ src/main/java/br/com/instrumental_rental/controller/api/ContactsAPI.java
@@ -0,0 +1,57 @@
+package br.com.instrumental_rental.controller.api;
+
+import br.com.instrumental_rental.Mappers.IContactsMapper;
+import br.com.instrumental_rental.controller.IContactAPI;
+import br.com.instrumental_rental.controller.dto.requests.ContactsDTO;
+import br.com.instrumental_rental.controller.dto.responses.responses.ContactsListResponseDTO;
+import br.com.instrumental_rental.controller.dto.responses.responses.ContactsResponseDTO;
+import br.com.instrumental_rental.controller.dto.responses.responses.DeleteResponseDTO;
+import br.com.instrumental_rental.exceptions.ContactNotFoundException;
+import br.com.instrumental_rental.service.interfaces.IContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("V1/contacts")
+public class ContactsAPI implements IContactAPI{
+
+    private final IContactService contactService;
+
+    private final IContactsMapper contactsMapper;
+
+    @Autowired
+    public ContactsAPI(IContactService contactService, IContactsMapper contactsMapper) {
+        this.contactService = contactService;
+        this.contactsMapper = contactsMapper;
+    }
+
+    @Override
+    public ContactsResponseDTO add(ContactsDTO contactsDTO) {
+        return ContactsResponseDTO.builder()
+                .data(
+                        contactsMapper.convertToDTO(
+                                contactService.save(
+                                        contactsMapper.convertToEntity(
+                                                contactsDTO)
+                                )
+                        )
+                ).build();
+    }
+
+    @Override
+    public ContactsListResponseDTO find(String holder) throws ContactNotFoundException {
+        return null;
+    }
+
+    @Override
+    public ContactsResponseDTO update(Long contactsId) throws ContactNotFoundException {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<DeleteResponseDTO> delete(Long contactsId) throws ContactNotFoundException {
+        return null;
+    }
+}
