59e42224e6dd5763c94375e6e4c2905eda74d708
MrMaia <mrmaiacodes@gmail.com>
2024-04-15 21:53:44 -0300

    inserted IContactsMapper and ContactsAPI mock annotations

--- src/test/java/br/com/instrumental_rental/api/ContactsAPITest.java
+++ src/test/java/br/com/instrumental_rental/api/ContactsAPITest.java
@@ -1,9 +1,18 @@
 package br.com.instrumental_rental.api;
 
+import br.com.instrumental_rental.Mappers.IContactsMapper;
+import br.com.instrumental_rental.controller.api.ContactsAPI;
 import br.com.instrumental_rental.service.interfaces.IContactService;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 public class ContactsAPITest {
     @Mock
     IContactService contactService;
+
+    @Mock
+    IContactsMapper contactMapper;
+
+    @InjectMocks
+    ContactsAPI contactsAPI;
 }
