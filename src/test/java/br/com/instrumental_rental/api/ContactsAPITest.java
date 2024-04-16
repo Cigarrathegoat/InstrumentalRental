318a5f6724994e031f5675f34043faf773c38b38
MrMaia <mrmaiacodes@gmail.com>
2024-04-15 21:52:10 -0300

    inserted contactService mock annotation

--- src/test/java/br/com/instrumental_rental/api/ContactsAPITest.java
+++ src/test/java/br/com/instrumental_rental/api/ContactsAPITest.java
@@ -1,6 +1,9 @@
 package br.com.instrumental_rental.api;
 
+import br.com.instrumental_rental.service.interfaces.IContactService;
+import org.mockito.Mock;
+
 public class ContactsAPITest {
-
-
+    @Mock
+    IContactService contactService;
 }
