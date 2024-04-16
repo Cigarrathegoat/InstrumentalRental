c55a4bfd12cb73c5c58d747f26ae1736b014f79b
MrMaia <mrmaiacodes@gmail.com>
2024-04-15 22:03:06 -0300

    created contactDTOBuilder builder class

--- src/test/java/br/com/instrumental_rental/api/ContactsAPITest.java
+++ src/test/java/br/com/instrumental_rental/api/ContactsAPITest.java
@@ -2,9 +2,13 @@ package br.com.instrumental_rental.api;
 
 import br.com.instrumental_rental.Mappers.IContactsMapper;
 import br.com.instrumental_rental.controller.api.ContactsAPI;
+import br.com.instrumental_rental.models.ContactBuilder;
 import br.com.instrumental_rental.service.interfaces.IContactService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 public class ContactsAPITest {
     @Mock
@@ -15,4 +19,28 @@ public class ContactsAPITest {
 
     @InjectMocks
     ContactsAPI contactsAPI;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    /*@Test
+    void testAddSuccess() {
+        var attendantNoId = AttendantBuilder.attendantBuilderNoId();
+        var attendantBuilder = AttendantBuilder.attendantBuilder();
+        var attendantDTONoIdBuilder = AttendantDTOBuilder.attendantDTONoIdSuccessBuilder();
+        var attendantDTOBuilder = AttendantDTOBuilder.attendantDTOSuccessBuilder();
+        when(attendantMapper.convertToEntity(attendantDTONoIdBuilder)).thenReturn(attendantNoId);
+        when(attendantService.save(attendantNoId)).thenReturn(attendantBuilder);
+        when(attendantMapper.convertToDTO(attendantBuilder)).thenReturn(attendantDTOBuilder);
+        AttendantResponseDTO result = attendantAPI.add(attendantDTONoIdBuilder);
+        Assertions.assertEquals(AttendantResponseDTO.builder().data(attendantDTOBuilder).build(), result);
+    }
+     */
+    @Test
+    void testAddSuccess() {
+        var contactNoId = ContactBuilder.contactNoIdBuilder();
+        var contact
+    }
 }
---
+++ src/test/java/br/com/instrumental_rental/models/ContactDTOBuilder.java
@@ -0,0 +1,21 @@
+package br.com.instrumental_rental.models;
+
+import br.com.instrumental_rental.controller.dto.requests.ContactsDTO;
+import br.com.instrumental_rental.repository.entities.Contact;
+
+public class ContactDTOBuilder {
+
+    public static ContactsDTO contactBuilder() {
+        return ContactsDTO.builder()
+                .contactsId(1L)
+                .contactType("email")
+                .contactContent("contact@gmail.com").build();
+    }
+
+    public static ContactsDTO contactNoIdBuilder() {
+        return ContactsDTO.builder()
+                .contactsId(null)
+                .contactType("email")
+                .contactContent("contact@gmail.com").build();
+    }
+}
