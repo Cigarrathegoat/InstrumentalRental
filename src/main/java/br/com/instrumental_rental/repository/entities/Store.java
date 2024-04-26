c22fcdb7c676fb7a9894c07c25191571a6c49e2d
MrMaia <mrmaiacodes@gmail.com>
2024-04-25 22:14:50 -0300

    wrote annotations on Store entity class

--- src/main/java/br/com/instrumental_rental/repository/entities/Store.java
+++ src/main/java/br/com/instrumental_rental/repository/entities/Store.java
@@ -1,4 +1,32 @@
 package br.com.instrumental_rental.repository.entities;
 
+
+/*
+@Entity
+@Table(name = "TB_THEADDRESS", schema = "instrumental_rental")
+@SequenceGenerator(name = "SEQ_THEADDRESS")
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+ */
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_STORE", schema = "instrumental_rental")
+@SequenceGenerator(name = "SEQ_STORE")
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Store {
 }
