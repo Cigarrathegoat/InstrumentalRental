fca70ce8bc94671c3db1615df5d2c5c89ce4607d
MrMaia <mrmaiacodes@gmail.com>
2024-04-26 09:26:23 -0300

    created IStoreRepository interface

---
+++ src/main/java/br/com/instrumental_rental/repository/interfaces/IStoreRepository.java
@@ -0,0 +1,7 @@
+package br.com.instrumental_rental.repository.interfaces;
+
+import br.com.instrumental_rental.repository.entities.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IStoreRepository extends JpaRepository<Store,Long> {
+}
