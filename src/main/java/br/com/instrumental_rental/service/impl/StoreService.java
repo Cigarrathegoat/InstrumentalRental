3a0ae19a3cf3dee0c5ff50c201cc4fe2749e1dc9
MrMaia <mrmaiacodes@gmail.com>
2024-04-26 09:35:23 -0300

    did saveFirstTime service method on storeService class

--- src/main/java/br/com/instrumental_rental/service/impl/StoreService.java
+++ src/main/java/br/com/instrumental_rental/service/impl/StoreService.java
@@ -1,7 +1,40 @@
 package br.com.instrumental_rental.service.impl;
 
+import br.com.instrumental_rental.repository.entities.Store;
+import br.com.instrumental_rental.repository.interfaces.IStoreRepository;
+import br.com.instrumental_rental.service.interfaces.IStoreService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
-public class StoreService {
+public class StoreService implements IStoreService {
+
+
+    IStoreRepository storeRepository;
+
+    @Autowired
+    private StoreService(IStoreRepository storeRepository) {
+        this.storeRepository = storeRepository;
+    }
+    @Override
+    public List<Store> saveFirstTime(List<Store> storeList) throws StoreNotFoundException {
+        if(storeList.isEmpty()) throw StoreNotFoundException("S01", "Store not found");
+        else {
+            List<Store> savedStoreList = new ArrayList<>();
+            for (Store store : storeList) {
+                storeRepository.save(store);
+                savedStoreList.add(store);
+            }
+            return savedStoreList;
+        }
+
+    }
+
+    @Override
+    public Store save(Store store) {
+        return null;
+    }
 }
--- src/main/java/br/com/instrumental_rental/service/interfaces/IStoreService.java
+++ src/main/java/br/com/instrumental_rental/service/interfaces/IStoreService.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface IStoreService {
 
     List<Store> saveFirstTime(List<Store> storeList);
+
+    Store save(Store store);
 }
