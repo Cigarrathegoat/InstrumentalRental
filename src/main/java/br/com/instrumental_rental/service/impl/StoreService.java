fdeb242d4548e714d52c60584a5dd1cb46c14518
MrMaia <mrmaiacodes@gmail.com>
2024-04-26 09:28:47 -0300

    did IStoreService interface and StoreService impl classes

---
+++ src/main/java/br/com/instrumental_rental/service/impl/StoreService.java
@@ -0,0 +1,7 @@
+package br.com.instrumental_rental.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StoreService {
+}
---
+++ src/main/java/br/com/instrumental_rental/service/interfaces/IStoreService.java
@@ -0,0 +1,10 @@
+package br.com.instrumental_rental.service.interfaces;
+
+import br.com.instrumental_rental.repository.entities.Store;
+
+import java.util.List;
+
+public interface IStoreService {
+
+    List<Store> saveFirstTime(List<Store> storeList);
+}
