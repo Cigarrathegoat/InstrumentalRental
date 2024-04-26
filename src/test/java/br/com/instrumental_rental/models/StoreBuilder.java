22d3b805089606add37390a87ffcdb9c53420ac2
MrMaia <mrmaiacodes@gmail.com>
2024-04-26 13:37:55 -0300

    built StoreBuilder test class

--- src/test/java/br/com/instrumental_rental/models/StoreBuilder.java
+++ src/test/java/br/com/instrumental_rental/models/StoreBuilder.java
@@ -1,6 +1,24 @@
 package br.com.instrumental_rental.models;
 
+import br.com.instrumental_rental.repository.entities.*;
+
+import java.util.List;
+
 public class StoreBuilder {
 
+    //public final idNumber = 1L;
 
+    public static Store storeNoIdBuilder() {
+        return Store.builder().attendants(List.of(AttendantBuilder.attendantBuilder()))
+                .customers(List.of(CustomerBuilder.customerBuilder()))
+                .instruments(List.of(InstrumentBuilder.instrumentBuilder()))
+                .theAddress(TheAddressBuilder.theAddressBuilder()).build();
+    }
+
+    public static Store storeBuilder() {
+        return Store.builder().storeId(1L).attendants(List.of(AttendantBuilder.attendantBuilder()))
+                .customers(List.of(CustomerBuilder.customerBuilder()))
+                .instruments(List.of(InstrumentBuilder.instrumentBuilder()))
+                .theAddress(TheAddressBuilder.theAddressBuilder()).build();
+    }
 }
